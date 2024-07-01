b54843357c5c2de05067f8a25653389b48701ca7
Michael Catanzaro <catanzaromj@pm.me>
2024-07-01 08:33:32 -0400

    Fix versioning, packaging typos

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 import re
 
+
 def get_version():
     VERSIONFILE = "kmapper/_version.py"
     verstrline = open(VERSIONFILE, "rt").read()
     VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
     mo = re.search(VSRE, verstrline, re.M)
     if mo:
-        verstr = mo.group(1)
+        return mo.group(1)
     else:
         raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-setuptools.setup(
-    version = get_version
-)
+
+setuptools.setup(version=get_version())
